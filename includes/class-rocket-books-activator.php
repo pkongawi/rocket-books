13d1bbfa00392d10a82e7252b79abef4132d9f37
Patrick Kongawi <pkongawi@gmail.com>
2020-01-06 15:34:07 -0600

    Loading the files

---
+++ includes/class-rocket-books-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://booksshelf.com
+ * @since      1.0.0
+ *
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/includes
+ * @author     Patrick Kongawi  <hello@patrickkongawi.com>
+ */
+class Rocket_Books_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
